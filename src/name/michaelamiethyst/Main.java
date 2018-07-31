9091016d6cfe66849368553ccbd1d7f246986308
Michael Peter Gower Amiethyst <designatevoid@gmail.com>
2018-07-31 12:03:09 -0700

    failing test: main does not compile.

--- src/name/michaelamiethyst/Main.java
+++ src/name/michaelamiethyst/Main.java
@@ -1,7 +1,26 @@
 package name.michaelamiethyst;
 
+import java.util.List;
+
 public class Main {
+
+    List<Node> nodes = new List<>();
+
     public static void main(String[] args) {
+        Main main = new Main();
+        main.dfs();
+    }
+
+    public void dfs() {
         System.out.println("Hello World");
+        initDfs();
+    }
+
+    public void initDfs() {
+        // name, link1, link2
+        createNode(0, 1, 2);
+        createNode(1, 2);
+        createNode(2, 0, 3);
+        createNode(3, 3);
     }
 }
